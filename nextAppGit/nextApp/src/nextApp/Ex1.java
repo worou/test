be18d72f42ecbd97047eee72272fccd64f44a6f2
Adimicool <adimicool@gmail.com>
2017-12-28 12:15:08 +0100

    premier commit
    Change-Id: I20b2aba7d56cc83bb5fc3aede65c64b5e129e1a8

--- nextAppGit/nextApp/src/nextApp/Ex1.java
+++ nextAppGit/nextApp/src/nextApp/Ex1.java
@@ -1,10 +1,16 @@
 package nextApp;
 
+import java.util.Scanner;
+
 public class Ex1 {
 
 	public static void main(String[] args) {
-
-		System.out.println("Hello world ! ");
+		System.out.println("Quel est votre nom ?\n");
+		Scanner sc = new Scanner(System.in);
+		String nom = sc.nextLine();
+		
+		System.out.println("");
+		System.out.println("Bonjour " + nom + ".");
 	}
 
 }
